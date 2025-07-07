e25779db8572f5ee4388b290f213539b59b127b0
Moneythepro <107262811+Moneythepro@users.noreply.github.com>
2025-07-08 04:39:52 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,14 @@
-<!-- Place in your index.html before app.js -->
-<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js"></script>
-<script>
-  const firebaseConfig = {
-    apiKey: "AIzaSyAynlob2NhiLZZ0Xh2JPXgAnYNef_gTzs4",
-    authDomain: "stringwasp.firebaseapp.com",
-    projectId: "stringwasp",
-    storageBucket: "stringwasp.appspot.com",
-    messagingSenderId: "974718019508",
-    appId: "1:974718019508:web:79e30ee86f15bf36b374e1"
-  };
-  firebase.initializeApp(firebaseConfig);
-  const auth = firebase.auth();
-  const db = firebase.firestore();
-</script>
+// firebase.js (Firebase 8)
+var firebaseConfig = {
+  apiKey: "AIzaSyAynlob2NhiLZZ0Xh2JPXgAnYNef_gTzs4",
+  authDomain: "stringwasp.firebaseapp.com",
+  projectId: "stringwasp",
+  storageBucket: "stringwasp.appspot.com",
+  messagingSenderId: "974718019508",
+  appId: "1:974718019508:web:79e30ee86f15bf36b374e1"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
