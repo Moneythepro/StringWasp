9f4c227214d004a71e4494b68958c677a879fcfd
Moneythepro <107262811+Moneythepro@users.noreply.github.com>
2025-07-08 01:50:42 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,13 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// Initialize Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyAynlob2NhiLZZ0Xh2JPXgAnYNef_gTzs4",
   authDomain: "stringwasp.firebaseapp.com",
   projectId: "stringwasp",
-  storageBucket: "stringwasp.firebasestorage.app",
+  storageBucket: "stringwasp.appspot.com",  // fixed typo: .app → .app**spot**
   messagingSenderId: "974718019508",
   appId: "1:974718019508:web:79e30ee86f15bf36b374e1"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db   = firebase.firestore();
