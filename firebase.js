2f053bf7566dbb774d28f1f05ec79f9cf7afb4d5
Moneythepro <107262811+Moneythepro@users.noreply.github.com>
2025-07-09 01:25:51 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,15 @@
-<!-- DO NOT use import/export syntax in firebase.js -->
-<script>
-  // Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyAynlob2NhiLZZ0Xh2JPXgAnYNef_gTzs4",
-    authDomain: "stringwasp.firebaseapp.com",
-    projectId: "stringwasp",
-    storageBucket: "stringwasp.appspot.com",
-    messagingSenderId: "974718019508",
-    appId: "1:974718019508:web:59fabe6306517d10b374e1"
-  };
+// Firebase V8 config - works with script includes
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  var db = firebase.firestore();
-  var auth = firebase.auth();
-</script>
+var firebaseConfig = {
+  apiKey: "AIzaSyAynlob2NhiLZZ0Xh2JPXgAnYNef_gTzs4",
+  authDomain: "stringwasp.firebaseapp.com",
+  projectId: "stringwasp",
+  storageBucket: "stringwasp.appspot.com",
+  messagingSenderId: "974718019508",
+  appId: "1:974718019508:web:59fabe6306517d10b374e1"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
