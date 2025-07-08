f1491eacba3e787c6449b745e10ba5f8fc1c6914
Moneythepro <107262811+Moneythepro@users.noreply.github.com>
2025-07-09 03:49:06 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,14 @@
-// Firebase config for Firebase v8 SDK (non-module)
-const firebaseConfig = {
-  apiKey: "AIzaSyAynlob2NhiLZZ0Xh2JPXgAnYNef_gTzs4",
-  authDomain: "stringwasp.firebaseapp.com",
-  projectId: "stringwasp",
-  storageBucket: "stringwasp.appspot.com",
-  messagingSenderId: "974718019508",
-  appId: "1:974718019508:web:59fabe6306517d10b374e1"
-};
-
+// Firebase v8 config (do not re-declare firebase if already loaded)
 if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp({
+    apiKey: "AIzaSyAynlob2NhiLZZ0Xh2JPXgAnYNef_gTzs4",
+    authDomain: "stringwasp.firebaseapp.com",
+    projectId: "stringwasp",
+    storageBucket: "stringwasp.appspot.com",
+    messagingSenderId: "974718019508",
+    appId: "1:974718019508:web:59fabe6306517d10b374e1"
+  });
 }
+
+const db = firebase.firestore();
+const auth = firebase.auth();
