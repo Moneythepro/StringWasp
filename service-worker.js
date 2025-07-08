7c84ada9024dadc547346f48abc3aa269f1f4813
Moneythepro <107262811+Moneythepro@users.noreply.github.com>
2025-07-09 03:21:57 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,22 @@
-const CACHE_NAME = "stringwasp-cache-v1";
-const ASSETS = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/app.js",
-  "/firebase.js",
-  "/manifest.json",
-  "/notif.mp3",
-  "/favicon.png"
-];
-
-// Install event
-self.addEventListener("install", event => {
-  console.log("[SW] Installed");
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS);
-    })
-  );
-});
-
-// Activate event
-self.addEventListener("activate", event => {
-  console.log("[SW] Activated");
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => {
-        if (k !== CACHE_NAME) return caches.delete(k);
-      }))
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("stringwasp-cache").then(cache =>
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/app.js",
+        "/firebase.js",
+        "/notif.mp3",
+        "/favicon.png",
+        "/manifest.json"
+      ])
     )
   );
 });
 
-// Fetch event
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(res => {
-      return res || fetch(event.request);
-    })
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
