5defdf8b8e1ab6c95e47f2926ca506aa823fa12c
Moneythepro <107262811+Moneythepro@users.noreply.github.com>
2025-07-09 01:27:17 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,51 +1,28 @@
 const CACHE_NAME = "stringwasp-v1";
-const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/app.js",
-  "/firebase.js",
-  "/manifest.json",
-  "/favicon.png",
-  "/notif.mp3"
+const urlsToCache = [
+  "./",
+  "index.html",
+  "style.css",
+  "app.js",
+  "firebase.js",
+  "manifest.json",
+  "notif.mp3",
+  "icon-192.png",
+  "icon-512.png"
 ];
 
-// Install Service Worker
-self.addEventListener("install", (event) => {
-  console.log("[ServiceWorker] Install");
+// Install SW
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("[ServiceWorker] Caching app shell");
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Activate Service Worker
-self.addEventListener("activate", (event) => {
-  console.log("[ServiceWorker] Activate");
-  event.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            console.log("[ServiceWorker] Removing old cache", key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-// Fetch from Cache or Network
-self.addEventListener("fetch", (event) => {
+// Fetch from cache/network
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((res) => {
-      return res || fetch(event.request);
-    }).catch(() => {
-      return new Response("Offline");
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
     })
   );
 });
