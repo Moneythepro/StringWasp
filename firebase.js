7467281af03d6e38b6be5ee0409c487d988fa2f8
Moneythepro <107262811+Moneythepro@users.noreply.github.com>
2025-07-09 03:20:50 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,12 @@
-// Initialize Firebase (Classic Firebase v8 style)
-var firebaseConfig = {
+// Firebase 8.x compatible (non-module)
+firebase.initializeApp({
   apiKey: "AIzaSyAynlob2NhiLZZ0Xh2JPXgAnYNef_gTzs4",
   authDomain: "stringwasp.firebaseapp.com",
   projectId: "stringwasp",
   storageBucket: "stringwasp.appspot.com",
   messagingSenderId: "974718019508",
   appId: "1:974718019508:web:59fabe6306517d10b374e1"
-};
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
+});
 
 const db = firebase.firestore();
 const auth = firebase.auth();
