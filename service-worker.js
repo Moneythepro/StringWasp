d9efafa0b1fee6ba93fc199e37a64f1585daa6ce
Moneythepro <107262811+Moneythepro@users.noreply.github.com>
2025-07-09 03:43:23 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,12 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("stringwasp-cache").then(cache =>
-      cache.addAll([
-        "/",
-        "/index.html",
-        "/style.css",
-        "/app.js",
-        "/firebase.js",
-        "/notif.mp3",
-        "/favicon.png",
-        "/manifest.json"
-      ])
-    )
-  );
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  console.log('Service Worker installed');
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
+self.addEventListener('activate', e => {
+  console.log('Service Worker activated');
+});
+
+self.addEventListener('fetch', e => {
+  // Optional: offline support here
 });
