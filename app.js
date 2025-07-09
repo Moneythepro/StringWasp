ebce425f6b953d30b02719983b751a41beb4bd82
Moneythepro <107262811+Moneythepro@users.noreply.github.com>
2025-07-09 16:47:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+// UUID generator (replaces external uuidv4.js dependency)
+function uuidv4() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11)
+    .replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+}
+
 // Firebase Initialization (Firebase 8 CDN assumed)
 const auth = firebase.auth();
 const db = firebase.firestore();
