d4ee7c69f8ef734a4997442061fd95681d17d0dc
Moneythepro <107262811+Moneythepro@users.noreply.github.com>
2025-07-21 16:29:36 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase v8 Classic Initialization for StringWasp v2
 const firebaseConfig = {
   apiKey: "AIzaSyAcUxoMaCV7H6CdT53KlxmQlY3nqBiLHb8",
   authDomain: "stringwaspv2.firebaseapp.com",
@@ -16,6 +9,10 @@ const firebaseConfig = {
   measurementId: "G-FM5KK7D695"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Initialize Firebase (v8 syntax)
+firebase.initializeApp(firebaseConfig);
+
+// Enable Firebase services
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
